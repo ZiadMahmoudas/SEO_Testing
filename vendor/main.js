a41f73a8fb7c6adca685763874136863c727046c
ZiadElmohager <ziadbobo78@gmail.com>
2025-06-14 11:28:15 +0300

    change

--- vendor/main.js
+++ vendor/main.js
@@ -6,27 +6,11 @@ navLinks.forEach((link) => {
 
   });
 });
-const btn =  document.querySelector("#Book")
-const btn1 =  document.querySelector("#Book1")
-const btn2 =  document.querySelector("#Book2")
-const btn3 =  document.querySelector("#Book3")
-const btn4 =  document.querySelector("#Book4")
-const btn5 =  document.querySelector("#Book5")
-btn.addEventListener("click",function(){
+const btn =  document.querySelectorAll("#Book");
+
+btn.forEach(buttons=>{
+buttons.addEventListener("click",function(){
   window.open('https://wa.me/966544778503', '_blank');
 })
-btn1.addEventListener("click",function(){
-  window.open('https://wa.me/966544778503', '_blank');
 })
-btn2.addEventListener("click",function(){
-  window.open('https://wa.me/966544778503', '_blank');
-})
-btn3.addEventListener("click",function(){
-  window.open('https://wa.me/966544778503', '_blank');
-})
-btn4.addEventListener("click",function(){
-  window.open('https://wa.me/966544778503', '_blank');
-})
-btn5.addEventListener("click",function(){
-  window.open('https://wa.me/966544778503', '_blank');
-})
+
